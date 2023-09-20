a14e620d1bfb439f443860e73fdc6fa7cc2d7ea7
elduke717 <spacematcha@proton.me>
2023-09-20 14:33:53 -0400

    API now works with chat feature

--- chat.py
+++ chat.py
@@ -7,52 +7,54 @@ import time
 # Load .env file
 load_dotenv()
 
-# Get API Keyy
+# Get API Key
 api_key = os.getenv("API_KEY")
 
-print(f'My API key is: {api_key}')
-
-
 # Define the API endpoint and headers
-
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 HEADERS = {
-    "Authorization": api_key,
+    "Authorization": f'Bearer {api_key}',
     "Content-Type": "application/json"
 }
 
-# construct the payload - a list of messages, where each message has a role
+user_prompt = input("What would you like to ask gpt-3.5-turbo?")
+
+
 def chat_with_gpt():
-    payload = {"messages": [{"role": "system", "content": "Thanks for helping."},
-                            {"role": "user", "content": "Who is the first president of Antarctica?"}]}
-
-
-    # sending the API request
-    response = requests.post(API_ENDPOINT, headers=HEADERS, json=payload)
-
-    # handling the API response:
-
-    data = json.loads(response.text)
-    reply = data['choices'][0]['message']['content']
-    print("ChatGPT's reply:", reply)
-
-    # response loop with initial messages for each role - an infinite loop to iteratively process sending the user message and receive responses
+    payload = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "system", "content": "Thanks for helping."},
+                     {"role": "user", "content": f'{user_prompt}'}]}
 
     while True:
         response = requests.post(API_ENDPOINT, headers=HEADERS, json=payload)
+
+        if response.status_code != 200:
+            error_data = json.loads(response.text)
+            print(
+                f"Error: {error_data.get('error', {}).get('message', 'Unknown error')}")
+            break
+
         data = json.loads(response.text)
-        reply = data['choices'][0]['message']['content']
-        print("ChatGPT's reply:", reply)
+
+        # Check if 'choices' exists in the response data
+        if 'choices' in data and data['choices']:
+            reply = data['choices'][0]['message']['content']
+            print(f"ChatGPT's reply: {reply}")
+        else:
+            print("Unexpected response:", data)
+            break
 
         user_input = input("Your message: ")
-        payload['messages'].append({"role": "user", "content": user_input})
 
-        user_input = input("Your message: ")
         if 'bye' in user_input.lower():
+            print("Exiting the chat. Goodbye!")
             break
 
         payload['messages'].append({"role": "user", "content": user_input})
 
-        time.sleep(2)  # Rate limiting
+        # Sleep to prevent rate limiting
+        time.sleep(2)
+
 
 chat_with_gpt()
